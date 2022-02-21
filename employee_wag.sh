253b89542ffdf17655a2702da56e0da5e58481ed
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-02-21 12:12:08 +0530

    added: part_time employee wage

--- employee_wag.sh
+++ employee_wag.sh
@@ -1,10 +1,20 @@
 #welcome  to Employee Wage Computation program
-#checking employee is present or absent
+#checking daily employee is present or absent
 	if(($((RANDOM%2))==0))
 	then
-		echo "present"
+		echo "daily_employee is present"
 		echo "todays wage:" $((20*8))
 	else
-		echo "absent"
+		echo "daily-emloyee is absent"
 		echo 	"todays wage is zero:"
 	fi
+#checking parttime employee is present or absent
+
+	if(($((RANDOM%2))==0))
+        then
+                echo "part_time employee is present"
+                echo "todays wage:" $((20*8))
+        else
+                echo "part_time employee is absent"
+                echo    "todays wage is zero:"
+        fi
