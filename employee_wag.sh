2979e4c523dbc4905759b60f41be386e00edc2a9
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-02-21 14:31:26 +0530

    added:displayed

--- employee_wag.sh
+++ employee_wag.sh
@@ -4,6 +4,7 @@
 echo "1-for regular employee, 2-for part time employee"
 read -p "enter the number to know about employee:" num
 monthly_wage=0
+weekend=$((20*8*10))
 i=0
 day=0
 hours=0
@@ -12,19 +13,18 @@ case $num in
 	do
 		if(($((RANDOM%2))==0))
 		then
-			echo "day $i regular_employee is present"
 				daily_wage=$((20*8))
 				monthly_wage=$((monthly_wage+$daily_wage))
-			echo "todays wage:" $daily_wage
+			echo "day $i   present   daily wage $daily_wage"
                			((day++))
                 		hours=$((hours+8))
 		else
-			echo "day $i regular_emloyee is absent"
-			echo 	"todays wage is zero:"
+			echo "day $i   absent   daily wage 0"
+
 		fi
 	done
 			echo "working hours in a month are: " $hours
-			echo " monthly wage of the regular employee is:" $monthly_wage
+			echo " monthly wage of the regular employee is:" $(($monthly_wage+$weekend))
 	;;
 #checking parttime employee is present or absent
 
@@ -32,19 +32,17 @@ case $num in
         do
 		if(($((RANDOM%2))==0))
         	then
-                	echo "day $i part_time employee is present"
                                 daily_wage=$((20*8))
                                 monthly_wage=$((monthly_wage+$daily_wage))
-                        echo "todays wage:" $((20*8))
+                        echo  "day $i   present   daily wage $daily_wage"
                                 ((day++))
                                 hours=$((hours+8))
 		else
-                	echo "day $i part_time employee is absent"
-                	echo    "todays wage is zero:"
+                	echo  "day $i   absent   daily wage 0"
         	fi
 	done
 			echo "woking hours in a month are:" $hours
-                        echo " monthly wage of the part time employee is:" $monthly_wage
+                        echo " monthly wage of the part time employee is:" $(($monthly_wage+$weeknd))
 	;;
 	*) echo "invalid input - number must be 0 or 1"
 esac
