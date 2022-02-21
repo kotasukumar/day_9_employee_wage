81f9bbffd81b2c574e1ffc242828a37f570cfac6
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-02-21 12:28:54 +0530

    added: case statment

--- employee_wag.sh
+++ employee_wag.sh
@@ -1,6 +1,10 @@
 #welcome  to Employee Wage Computation program
 #checking daily employee is present or absent
-	if(($((RANDOM%2))==0))
+echo "1-for daily employee, 2-for part time employee"
+read -p "enter the number to know about employee:" num
+case $num in
+
+	1) if(($((RANDOM%2))==0))
 	then
 		echo "daily_employee is present"
 		echo "todays wage:" $((20*8))
@@ -8,9 +12,10 @@
 		echo "daily-emloyee is absent"
 		echo 	"todays wage is zero:"
 	fi
+	;;
 #checking parttime employee is present or absent
 
-	if(($((RANDOM%2))==0))
+	2) if(($((RANDOM%2))==0))
         then
                 echo "part_time employee is present"
                 echo "todays wage:" $((20*8))
@@ -18,3 +23,6 @@
                 echo "part_time employee is absent"
                 echo    "todays wage is zero:"
         fi
+	;;
+	*) echo "invalid input - number must be 0 or 1"
+esac
