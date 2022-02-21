0d33e9f4af2790ca9db94e952f1b245e7140d3d1
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-02-21 13:26:58 +0530

    added: monthly wage

--- employee_wag.sh
+++ employee_wag.sh
@@ -1,28 +1,41 @@
 #welcome  to Employee Wage Computation program
-#checking daily employee is present or absent
-echo "1-for daily employee, 2-for part time employee"
-read -p "enter the number to know about employee:" num
-case $num in
 
-	1) if(($((RANDOM%2))==0))
-	then
-		echo "daily_employee is present"
-		echo "todays wage:" $((20*8))
-	else
-		echo "daily-emloyee is absent"
-		echo 	"todays wage is zero:"
-	fi
+#checking regular employee is present or absent
+echo "1-for regular employee, 2-for part time employee"
+read -p "enter the number to know about employee:" num
+monthly_wage=0
+case $num in
+	1) for((i=0;i<=20;i++))
+	do
+		if(($((RANDOM%2))==0))
+		then
+			echo "day $i regular_employee is present"
+				daily_wage=$((20*8))
+				monthly_wage=$((monthly_wage+$daily_wage))
+			echo "todays wage:" $daily_wage
+		else
+			echo "day $i regular_emloyee is absent"
+			echo 	"todays wage is zero:"
+		fi
+	done
+                	echo " monthly wage of the regular employee is:" $monthly_wage
 	;;
 #checking parttime employee is present or absent
 
-	2) if(($((RANDOM%2))==0))
-        then
-                echo "part_time employee is present"
-                echo "todays wage:" $((20*8))
-        else
-                echo "part_time employee is absent"
-                echo    "todays wage is zero:"
-        fi
+	2)  for((i=0;i<=20;i++))
+        do
+		if(($((RANDOM%2))==0))
+        	then
+                	echo "part_time employee is present"
+                                daily_wage=$((20*8))
+                                monthly_wage=$((monthly_wage+$daily_wage))
+                        echo "todays wage:" $((20*8))
+		else
+                	echo "part_time employee is absent"
+                	echo    "todays wage is zero:"
+        	fi
+	done
+                        echo " monthly wage of the part time employee is:" $monthly_wage
 	;;
 	*) echo "invalid input - number must be 0 or 1"
 esac
